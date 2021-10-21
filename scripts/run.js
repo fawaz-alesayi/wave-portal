981291f26d0b41eea52c5ac57a2e680fd9e12aa1
Fulayfel <fawaztsa@gmail.com>
2021-10-21 15:11:27 +0000

    simulate running the contract 📃

--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,26 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
+  
     console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+  
+    let waveCount;
+    waveCount = await waveContract.getTotalWaveCount();
+    
+    // Simulate us (owner) hitting the wave contract .wave() function
+    let waveTxn = await waveContract.wave();
+    waveTxn = await waveContract.unWave();
+    await waveTxn.wait();
+
+    // Simulate some wallet owner (randomPerson) hitting the contract .wave() function
+    waveTxn = await waveContract.connect(randomPerson).wave();
+
+    await waveTxn.wait();
+  
+    waveCount = await waveContract.getTotalWaveCount();
   };
   
   const runMain = async () => {
