386b915156d415680cb2c3a2d981007f76e291ba
Fulayfel <fawaztsa@gmail.com>
2021-10-29 07:45:08 +0000

    add license and fix typo 😊

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        Heyo! Fawaz here, wave at me a I dare you, I double-dare you!
+        Heyo! Fawaz here, wave at me I dare you, I double-dare you!
         </div>
 
         <button className="waveButton" onClick={wave}>
